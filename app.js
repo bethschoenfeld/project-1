22c294750fa65a4879aad79269afbd9a3744adac
bethschoenfeld <bethingraham3@gmail.com>
2017-12-12 15:25:33 -0500

    Added jquery link to HTML and document.ready in app.js.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
 var words = ['peony', 'sunflower'];
 
 function answerWord () {
-    for( i=0 , i = words.length, i++) {}
+    for (i = 0, len = words.length, text = ""; i < len; i++) {
+        text += words[i] + "<br>";
+    }
 }
+
+});
