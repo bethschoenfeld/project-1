019e1ebf8640962251c34f9c474d7607cb139658
bethschoenfeld <bethingraham3@gmail.com>
2017-12-13 11:16:23 -0500

    Re adjusting all the things

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 $(document).ready(function () {
     console.log("ready!");
 
-    $(`.letters`).children()
-
     let words = ['peony', 'sunflower'];
-    let text = "";
-    let i;
+    console.log(words)
 
     function answerWord() {
-        for (i = 0, len = words.length, text = ""; i < len; i++) {
-            text += words[i] + "<br>";
-        }
-    }
+        for(i=0; words.length < i; i++){
 
-});
+        }
+    };
