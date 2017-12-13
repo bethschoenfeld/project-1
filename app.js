db27d416bb0c3632853a0cd9fc56501edf0d5976
bethschoenfeld <bethingraham3@gmail.com>
2017-12-13 15:50:45 -0500

    Added font white to css for answer and deleted "used letters" and "unused letters" divs.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     console.log("ready!");
 
-    const words = 'peony';
+    const words = 'sunflower';
     console.log(words)
     const answerSplit = words.split(``)
     console.log(answerSplit)
@@ -11,13 +11,15 @@ $(document).ready(function () {
     $(`.letters`).click(function (event) {
         const clickedLetter = event.target.textContent
         console.log(clickedLetter)
-        if (answerSplit.indexOf(clickedLetter) >= 0 ) {
-            $(event.target).css('color','gray')    
+        if (answerSplit.indexOf(clickedLetter) >= 0) {
+            $(event.target).css('color', 'gray')
         }
         else {
-           $(event.target).css('color', 'red')
-       };
+            $(event.target).css('color', 'red')
+        };
     })
-    
+
+    //When correct letter is clicked change color to black. 
+
 
 });
